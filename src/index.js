2dfdf584b3d1b1416ebfaf1fbd2640209b466c12
Nekorsis <Horick.94@mail.ru>
2017-07-05 02:14:00 +0700

    Added basic playability to the game

--- src/index.js
+++ src/index.js
@@ -2,13 +2,28 @@ import './index.css';
 import './reset.css';
 
 const gameContainer = document.getElementsByClassName('game-field')[0];
-const listOfCircles = [];
+const listOfCircleIds = [];
+let timer;
+
+const circleButton = document.getElementsByClassName('add-circle-btn')[0];
+circleButton.onclick = () => {
+    createCircle();
+}
+
+const startGameButton = document.getElementsByClassName('start-game-btn')[0];
+startGameButton.onclick = () => {
+    startGame();
+}
+
+const endGameButton = document.getElementsByClassName('end-game-btn')[0];
+endGameButton.onclick = () => {
+    stopGame();
+}
 
 const createCircle = () => {
     const getRandomCoordinates = () => {
-        const containerSize = {w: gameContainer.clientWidth - 40, h: gameContainer.clientHeight - 40};
+        const containerSize = {w: gameContainer.clientWidth - 60, h: gameContainer.clientHeight - 60};
         const getRandomArbitrary = (min, max) => {
-            console.log('max: ', max);
             return Math.round(Math.random() * (max - min) + min);
         }
         return {
@@ -16,12 +31,39 @@ const createCircle = () => {
             y: getRandomArbitrary(40, containerSize.h),
         }
     }
+    /* Initialize circle */
     const circle = document.createElement('div');
     circle.className = 'circle';
+
+    /* Set circle id and push it to array to kepp track of it */
+    const circleId = Math.round(Math.random() * (100000 - 1) + 1);
+    circle.id = circleId;
+    listOfCircleIds.push(circle.id);
+
+    /* Setting onclick property */
+    circle.onclick = () => {
+        listOfCircleIds.forEach((element, index) => {
+            if (element === circle.id) {
+            }
+        })
+        circle.remove();
+    }
+
+    /* Mount circle in a random place */
     const coordinates = getRandomCoordinates();
-    console.log('coordinates: ', coordinates);
     circle.style.left = coordinates.x +'px';
     circle.style.top = coordinates.y +'px';
     gameContainer.appendChild(circle);
 };
-createCircle();
+
+const startGame = () => {
+    timer = setInterval(() => {
+        createCircle();
+    }, 1000);
+}
+
+const stopGame = () => {
+    clearInterval(timer);
+}
+
+// createCircle();
